086b990e61dd516e6a7fb30299135e75ea7ceb56
Van Pham <vanpham@gmail.com>
2014-09-24 03:56:06 -0700

    Update vast.js

--- assets/js/vast.js
+++ assets/js/vast.js
@@ -3,7 +3,6 @@
         var $video;
         var video;
         var isPlaying, isClosed, didClick, closeButtonShown, trackAt, isFinished, noSkippable, endTime;
-        var DEBUG = true;
 
     AppSponsorPlayer.init = function (forceNoSkippable, jumpToTime) {
         $video = $("#appSponsorVideoTag");
@@ -49,36 +48,35 @@
 
         $video.on('timeupdate', function (e) {
             if (goToFrame) return; //do not allow to trigger event tracking again
-                  
+            
             var currentTime = video.currentTime;
-            if (!noSkippable  && currentTime >= timeToShowCloseButton(video) && !closeButtonShown) {
+
+            console.log("===============================");
+            console.log("!noSkippable: " + !noSkippable);
+            console.log("currentTime: " + currentTime);
+            console.log("timeToShowCloseButton(video): " + timeToShowCloseButton(video));
+            console.log("!closeButtonShown: " + closeButtonShown);
+            console.log("===============================");
+
+            if (!noSkippable  && (currentTime >= timeToShowCloseButton(video)) && !closeButtonShown) {
                 showCloseButton('showCloseButtonTimer');
             }  else if(video.hasOwnProperty('duration')) {
                 if (trackAt < 75 && currentTime >= (0.75 * video.duration)) {
+                    console.log('---videoStateTrack75---');
                     executeNativeCall('videoStateTrack75');
                     trackAt = 75;
-                    if(DEBUG){
-                        console.log('videoStateTrack75');
-                    }
                 } else if (trackAt < 50 && currentTime >= (0.50 * video.duration)) {
+                    console.log('---videoStateTrack50---');
                     executeNativeCall('videoStateTrack50');
                     trackAt = 50;
-                    if(DEBUG){
-                        console.log('videoStateTrack50');
-                    }
                 } else if (trackAt < 25 && currentTime >= (0.25 * video.duration)) {
+                    console.log('---videoStateTrack25---');
                     executeNativeCall('videoStateTrack25');
                     trackAt = 25;
-                    if(DEBUG){
-                        console.log('videoStateTrack25');
-                    }
                 } else {
                     setTimeout(function () { //handle edge case when video stall that video currentTime has not changed much
                         if (isPlaying && !closeButtonShown && Math.abs(currentTime - video.currentTime) < 0.01) {
                             showCloseButton('showCloseButtonStall');
-                            if(DEBUG){
-                                console.log('videoStateTrack75');
-                            }                            
                         }
                     }, 1000);
                 }
@@ -103,9 +101,6 @@
             video.play();
         } else {
             showCloseButton('playVideoFailed');
-            if(DEBUG){
-                console.log('ready - playVideoFailed');
-            }
 	    }
     };
 
@@ -115,36 +110,22 @@
             video.play();
         } else {
             showCloseButton('playVideoFailed');
-            if(DEBUG){
-                console.log('lastFrame - playVideoFailed');
-            }
-        }
+	    }
     };
 
     var timeToShowCloseButton = function(video) {
-        if(DEBUG){
-            console.log('timeToShowCloseButton');
-            if(video.hasOwnProperty('duration')) {
-            	console.log(video);
-            }
-        }
         if(video.hasOwnProperty('duration')) {
             if(video.duration > 16) { //if it is long video, show close btn after 5 sec
-            	
                 return 5;
             } else { //video length is less than 15 sec
                 return 300; //should not show close btn at all
             }
         } else {
-            return 12; //edge case, should not happen
+            return 5; //edge case, should not happen
         }
     };
 
     var showCloseButton = function(action){
-        if(DEBUG){
-            console.log('showCloseButton');
-            console.log('action: ' + action);
-        }
         if (!closeButtonShown) {
             executeNativeCall(action);
             closeButtonShown = true;
